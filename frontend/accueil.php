b8eb1ec874cbab435d484b9b6172b5d1a7942deb
devinou971 <damiendasseux@gmail.com>
2023-01-18 11:14:26 +0100

    Added welcome page

---
+++ frontend/accueil.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    Bienvenu sur ce site. Connectez-vous et vous pourrez acceder aux services : 
+    <a href="login.php">Se connecter</a>
+</body>
+</html>
